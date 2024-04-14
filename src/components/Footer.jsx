d8a03d36e6a7191309e4123421b1a562e9ab8abb
ashish <ashishdubbewar20@gmail.com>
2024-04-14 12:41:32 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='text-center'>
+      Copyright &#169; 2024 Ashish Dubbewar. All rights reserved
+    </div>
+  )
+}
+
+export default Footer
