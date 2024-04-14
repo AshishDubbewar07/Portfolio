9e8e24de6847009d3d801f9956cf0cddaee0727a
ashish <ashishdubbewar20@gmail.com>
2024-04-14 12:49:27 +0530

    first commit

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -57,6 +57,7 @@ const Skills = () => {
     <div id="Skills">
       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-center">
         <div>
+
           <p className="text-4xl font-bold border-b-4 border-gray-400 p-2 inline">
             Skills
           </p>
